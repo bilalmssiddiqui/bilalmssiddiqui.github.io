8ae70142af319ed82bff75d89089b76b3daf08dc
bilalmssiddiqui <bilalmssiddiqui@gmail.com>
2025-11-07 11:42:11 +0100

    Removed speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
@@ -11,7 +10,6 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
       <Analytics />
-      <SpeedInsights/>
     </BrowserRouter>
   </React.StrictMode>
 );
